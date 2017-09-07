7224099a250680f64ee9f9dd2d41677310cb7da1
Daniel Andrade <dca650@gmail.com>
2017-09-07 20:55:38 -0300

    Inserting books from view

--- app/infra/ProductsDAO.js
+++ app/infra/ProductsDAO.js
@@ -6,6 +6,10 @@ ProductsDAO.prototype.list = function(callback){
     this._dbConnection.query("SELECT * FROM products", callback);
 }
 
+ProductsDAO.prototype.insert = function(product, callback){
+    this._dbConnection.query("INSERT INTO products SET ?", product, callback);
+}
+
 module.exports = function(){
    return ProductsDAO;
 }
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
-    app.get("/products", function(request, response){
+    var listProducts = function(request, response){
         var connection = app.infra.connectionFactory();
         var productsDAL = new app.infra.ProductsDAO(connection);
 
@@ -10,6 +10,24 @@ module.exports = function(app){
         });
 
         connection.end();
-        //response.render("produtos/lista");
+    };
+
+    app.get("/products", listProducts);
+
+    app.get("/products/form", function(request, response){
+        response.render("products/form");
+    });
+
+    app.post("/products", function(request, response){
+        var product = request.body;
+
+        var connection = app.infra.connectionFactory();
+        var productsDAO = new app.infra.ProductsDAO(connection);
+        
+        productsDAO.insert(product, function(error, result){
+            response.redirect("/products");
+        });
+
+        connection.end();
     });
 }
